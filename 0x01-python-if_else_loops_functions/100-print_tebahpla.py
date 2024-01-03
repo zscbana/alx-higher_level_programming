576df1600f3ecf9f55abb983dac1001a55460b52
zscbana <zscbana@gmail.com>
2024-01-03 16:57:12 +0200

    correction of task 100 and task 101

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 for x in range(122, 96, -1):
     if x % 2 == 0:
         print("{:c}".format(x-32), end='')
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    for x in range(len(str)):
+        if x == n:
+            continue
