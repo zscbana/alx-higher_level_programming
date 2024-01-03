a81600c63d1772eae8b66dba9ece28844b9efa18
zscbana <zscbana@gmail.com>
2024-01-03 05:55:07 +0200

    correction of task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for x in range(10):
-    for i in range(10):
-        print("{:d}{:d}".format(x, i), end="\n" if x == 9 and i == 9 else ", ")
+    for i in range(x + 1, 10):
+        print("{:d}{:d}".format(x, i), end=", " if x != 8 or i != 9 else "\n")
