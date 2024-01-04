d6cda48f25ed38265df76d5db15ad1d090f67ec2
zscbana <zscbana@gmail.com>
2024-01-05 01:55:37 +0200

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    
+
+    c = len(sys.argv) - 1
+    if c == 0:
+        print("0 arguments")
+    elif c == 1:
+        print("1 argument")
+    else:
+        print("{} arguments".format(c))
+    for i in range(c):
+        print("{}: {}".format(i + 1, sys.argv(i + 1)))
