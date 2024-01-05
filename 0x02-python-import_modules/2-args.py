6b89dd66fed1cfa9bca37b06ea7c46ab54bcc70d
zscbana <zscbana@gmail.com>
2024-01-05 02:03:04 +0200

    task 2.3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     elif c == 1:
         print("1 argument:")
     else:
-        print("{} arguments;".format(c))
+        print("{} arguments:".format(c))
     for i in range(c):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
