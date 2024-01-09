08fa647cd1864a5eefeac7a6906f6ed2c732a8d1
zscbana <zscbana@gmail.com>
2024-01-09 03:10:42 +0200

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,6 +2,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     x1 = tuple_a[0] if len(tuple_a) > 0 else 0
     x2 = tuple_a[1] if len(tuple_a) > 1 else 0
-    z1 = tuple_b[0] if len(tuple_a) > 0 else 0
-    z2 = tuple_b[1] if len(tuple_a) > 1 else 0
+    z1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    z2 = tuple_b[1] if len(tuple_b) > 1 else 0
     return (x1 + z1, x2 + z2)
