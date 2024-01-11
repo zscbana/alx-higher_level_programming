b18e578328c2d7d754be9bd3fab467db30e413b7
omar17-b <oelbanna17@gmail.com>
2024-01-11 04:53:03 +0200

    task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     for x in sorted(a_dictionary.keys()):
-        print("{}: {}".format(x, a_dictionary[x] * 2))
+        print("{}: {}".format(x, a_dictionary.value * 2))
