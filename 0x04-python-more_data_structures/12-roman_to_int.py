0cf7004149f548eb0210dfc7ac8c2af5167a2bfa
omar17-b <oelbanna17@gmail.com>
2024-01-11 05:17:43 +0200

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,13 +5,13 @@ def roman_to_int(roman_string):
     total = 0
     num = 0
     dir = {
-        'I' : 1,
-        'V' : 1,
-        'X' : 10,
-        'L' : 50,
-        'C' : 100,
-        'D' : 500,
-        'M' : 1000
+        'I': 1,
+        'V': 1,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     }
     for x in reversed(roman_string):
         num = dir(x)
