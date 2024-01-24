41fdd736824e9d219d1f855fe4a7671560e4f186
omar17-b <oelbanna17@gmail.com>
2024-01-24 03:32:13 +0200

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-class square:
+"""Define class Square"""
+
+
+class Square:
+    """a square"""
     def __init__(self, size):
+        """Initialize a Square.
+        Args:
+            size : The size of the new square.
+        """
         self.__size = size
