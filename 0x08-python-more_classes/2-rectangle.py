e8a4ea7a957d3bdb4d320a3cd3ee1de9c17492c0
omar17-b <oelbanna17@gmail.com>
2024-01-30 02:16:52 +0200

    task2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,28 @@
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        self.__height = value
