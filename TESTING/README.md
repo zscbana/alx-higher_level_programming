e291e75d1f0849f45d53e2adbd6c714f9ca65316
zscbana <zscbana@gmail.com>
2024-02-06 09:40:14 +0200

    .

--- TESTING/README.md
+++
@@ -1,1 +0,0 @@
-# testing and practicing file
--- TESTING/app.py
+++
@@ -1,7 +0,0 @@
-from Employee import Employee
-omar = Employee("omar", 21, 12200028, 6)
-zoza = Employee("zoza", 15, 12200023, 2)
-
-print("omar id = {:d} and zoza id = {:d}".format(omar.id, zoza.id))
-print(omar.Rating())
-print(zoza.Rating())
