a93b6f7d09a668082bcd77f8021eea3e3d7f7c22
zscbana <zscbana@gmail.com>
2024-02-06 09:58:57 +0200

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module for mylist class. """
+
+
+class MyList:
+    """ coustem my list class."""
+    def print_sorted(self):
+        "Method print the sorted list"
+        print(sorted(self))
