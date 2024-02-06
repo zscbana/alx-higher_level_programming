dcf8a3092984f96ddf2a2cce9ac5a7d787afc8cb
zscbana <zscbana@gmail.com>
2024-02-06 14:31:12 +0200

    task 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Module for Myint Class. """
+
+
+class MyInt(int):
+    """ Class representing rebel. """
+    pass
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" function that adds a new attribute to an object. """
+
+
+def add_attribute(mc, ClassName, Name):
