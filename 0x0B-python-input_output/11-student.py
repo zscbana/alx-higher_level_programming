eeb3a48c1dc3f00c72179bd7ef7ae107d6eb90a2
zscbana <zscbana@gmail.com>
2024-02-07 00:41:07 +0200

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" class Student """
+
+
+class Student:
+    """ A representation of a student."""
+    def __init__(self, first_name, last_name, age):
+        """"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns a dictionary representation of a Student instance. """
+        try:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return self.__dict__
+        except Exception:
+            return self.__dict__
+        my_dict = dict()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                my_dict[key] = value
+        return my_dict
+
+    def reload_from_json(self, json):
+        """Reload from json """
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
