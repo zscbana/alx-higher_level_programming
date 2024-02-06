14fbd987685c1498878a45b70bb9ad6f9f70f4ec
zscbana <zscbana@gmail.com>
2024-02-06 23:02:16 +0200

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a script that adds all arguments to
+Python list, and then save them to a file:"""
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+arglist = list(sys.argv[1:])
+
+try:
+    old_data = load_from_json_file("add_item.json")
+except Exception:
+    old_data = []
+
+old_data.extend(arglist)
+save_to_json_file(old_data, "add_item.json")
