fbaa69fa1472761d131bf38eff2b11a1c761c746
zscbana <zscbana@gmail.com>
2024-02-07 00:19:17 +0200

    task 9

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""task 7."""
+"""Script to add command line arguments to a JSON file."""
 
 import sys
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
