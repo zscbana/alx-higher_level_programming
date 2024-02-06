49e003050c3af1b38122c983e84f8c3b8ae6703a
zscbana <zscbana@gmail.com>
2024-02-06 09:52:11 +0200

    task 0.2

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+""" Module for lookup method """
+
+
 def lookup(obj):
+    '''Looks up
+    Args:
+        obj: object to list
+    Return:
+        list:of attributes
+    '''
     return dir(obj)
