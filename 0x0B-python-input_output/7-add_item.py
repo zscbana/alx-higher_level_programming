7166013ebed6d0424a6f083697df904526594b45
zscbana <zscbana@gmail.com>
2024-02-06 23:13:25 +0200

    task 7.4

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,12 +14,15 @@ load_from_json_file = load_module.load_from_json_file
 
 save_to_json_file = import_module("5-save_to_json_file").save_to_json_file
 
+
 if not exists("add_item.json"):
     with open("add_item.json", "w") as file:
         file.write("[]")
 
+
 my_list = load_from_json_file("add_item.json")
 
 my_list.extend(sys.argv[1:])
+
 
 save_to_json_file(my_list, "add_item.json")
