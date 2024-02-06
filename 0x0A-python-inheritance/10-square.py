c9b49687d4d591ed29b1051aba2776fa3cbbc058
zscbana <zscbana@gmail.com>
2024-02-06 11:02:58 +0200

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 """ Module for Square SubClass. """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class representing a square figure. """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area():
+        '''Area of squre'''
+        return self.__size ** 2
