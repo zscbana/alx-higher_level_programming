b77b0abfa59698a2feaffb486c910c24242a16cd
zscbana <zscbana@gmail.com>
2024-02-13 03:09:36 +0200

    task 2.2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,8 @@ from models.base import Base
 class Rectangle(Base):
     """define class Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        super.__init__(id)
+        """Constructors"""
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
