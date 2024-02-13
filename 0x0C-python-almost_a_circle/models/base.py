54ad1667277dffafcc014fd0eaf7b88a50a1595b
zscbana <zscbana@gmail.com>
2024-02-13 04:33:14 +0200

    task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,3 +32,10 @@ class Base:
                 obj_dicts = [obj.to_dictionary() for obj in list_objs]
                 json_string = cls.to_json_string(obj_dicts)
                 file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string:
+            json.loads(json_string)
+        else:
+            return []
